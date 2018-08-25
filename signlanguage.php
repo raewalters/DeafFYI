ac9cbf1b9677de4a421a87cefc747542b6f154be
raewalters <rachel.marie1184@gmail.com>
2018-08-25 18:05:22 -0400

    Create signlanguage.php

---
+++ signlanguage.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <title>Sign Language | DeafFYI</title>
+  <meta name="description" content="On the sign language page learn facts and history of American Sign Language.">
+  <link rel="stylesheet" href="css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+		  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+		  <script src="http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.js"
+		  type="text/javascript"></script>
+
+		  <style type="text/css">
+		  @import url("http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.css");
+
+		  #feedControl {
+		  margin-top : 5px;
+		  margin-left: 0px;
+		  margin-right: 0px;
+		  width: 300px;
+		  font-size: 10px;
+		  color: #000000;
+		  }
+		  </style>
+		  <script type="text/javascript">  
+		  function load() {
+		  var feed ="http://news.google.com/news?cf=all&hl=en&pz=1&ned=us&q=Deaf&output=rss";
+		  new GFdynamicFeedControl(feed, "feedControl");
+
+		  } 
+		  google.load("feeds", "1");
+		  google.setOnLoadCallback(load);
+		  </script>
+
+  
+</head>
+
+<body>
+
+<header class="banner">
+
+	<div class="wrapper">
+
+	<div id="banner">
+		<div id="wrapper">
+			<div id="container">
+					<img class="banner-img" src="images/header.png" alt="Deaf FYI Logo"/>
+			</div>
+		</div>
+	</div>
+	</div>
+	
+<nav class="primary-menu">
+   <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="people.php">Deaf People</a></li>
+      <li><a href="signlanguage.php">Sign Language</a></li>
+	  <li><a href="etiquette.php">Etiquette</a></li>
+	  <li><a href="resources.php">Resources</a></li>
+	  
+   </ul>
+</nav>
+
+   </header>
+
+<main class="main">
+
+<h2>American Sign Language</h2>
+<img class="banner-img" src="images/ASL.png" alt="ASL - hands spelling A S L. "/>
+
+<ul>
+<li><a href="#legend">Facebook Video -  Legend of Clerc and Gallaudet</a></li>
+<li><a href="#universal">Is sign language universal?</a></li>
+<li><a href="#ASL">What is American Sign Language?</a></li>
+<li><a href="#grammar">Does ASL have grammar?</a></li>
+<li><a href="#gesturing">Is ASL a form of gesturing?</a></li>
+<li><a href="#compare">How does ASL compare with spoken language?</a></li>
+<li><a href="#video">YouTube Video - Learn ASL Alphabet</a></li>
+<li><a href="#alphabet">American Sign Language Alphabet</a></li>
+</ul>
+
+
+<h3 id="legend">Legend of Clerc and Gallaudet</h3>
+To view captions: click the cog icon and check captions. 
+<div class="video-container"><iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fconvo%2Fvideos%2F10154777397781591%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" allowfullscreen></iframe></div>
+
+
+<h3 id="universal">Is sign language universal?</h3>
+<p>Sign Language is NOT universal, different countries have their own sign languages. No one form of sign language is 
+universal. For example, British Sign Language (BSL) is a different language from ASL, and Americans who know ASL may not 
+understand BSL.</p>
+
+<h3 id="ASL"> What is American Sign Language?</h3>
+<p>American Sign Language (ASL) is a visual language. With signing, the brain processes linguistic information through the 
+eyes. The shape, placement, and movement of the hands, as well as facial expressions and body movements, all play important 
+parts in conveying information. ASL is used predominantly in the United States and in many parts of Canada. ASL is accepted 
+by many high schools, colleges, and universities in fulfillment of modern and “foreign” language academic degree 
+requirements across the United States.</p>
+
+<h3 id="grammar">Does ASL have grammar?</h3>
+<p>Yes. ASL is NOT simply signing as you would speak English. ASL has its own grammar, syntax and sentence structure that is 
+different from English.</p>
+
+<h3 id="gesturing">Is ASL a form of gesturing?</h3>
+<p>No, Deaf people will gesture if they are trying to emphasize something or communicate without sign, gesturing and ASL 
+are NOT the same. Gesturing is not a language with rules of grammar and structure. ASL is a langage while gestures is a 
+game played at games around the holidays.</p>
+
+<h3 id="compare">How does ASL compare with spoken language?</h3>
+<p>In spoken language, words are produced by using the mouth and voice to make sounds. But for people who are deaf 
+(particularly those who are profoundly deaf), the sounds of speech are often not heard, and only a fraction of speech 
+sounds can be seen on the lips. Sign languages are based on the idea that vision is the most useful tool a deaf person 
+has to communicate and receive information.</p>
+<p>ASL is a language completely separate and distinct from English. It contains all the fundamental features of 
+language—it has its own rules for pronunciation, word order, and complex grammar. While every language has ways of 
+signaling different functions, such as asking a question rather than making a statement, languages differ in how this 
+is done. For example, English speakers ask a question by raising the pitch of their voice; ASL users ask a question by 
+raising their eyebrows, widening their eyes, and tilting their bodies forward.</p>
+<p>Just as with other languages, specific ways of expressing ideas in ASL vary as much as ASL users do. In addition to 
+individual differences in expression, ASL has regional accents and dialects. Just as certain English words are spoken 
+differently in different parts of the country, ASL has regional variations in the rhythm of signing, form, and pronunciation.
+Ethnicity and age are a few more factors that affect ASL usage and contribute to its variety.</p>
+
+<h3 id="video">YouTube Video - Learn ASL Alphabet</h3>
+<div class="video-container"><iframe width="560" height="315" src="https://www.youtube.com/embed/lYhAAMDQl-Q" allowfullscreen></iframe></div>
+
+<h3 id="alphabet">American Sign Language Alphabet</h3>
+<img class="banner-img" src="images/ASLAlphabet.jpg" alt="ASL Alphabet"/>
+
+
+<strong> Resources </strong>
+<p><a href="http://theaslapp.com/faq" target="_blank">The ASL App - "Deaf Culture FAQ"</a></p>
+<p><a href="https://nad.org/issues/american-sign-language/what-is-asl" target="_blank">The National Association of the Deaf - "What is American Sign Language"</a></p>
+<p><a href="https://www.nidcd.nih.gov/health/american-sign-language" target="_blank">National Institute on Deafness and Other Communication Disorders - "American Sign Language"</a></p>
+
+</main>
+
+
+<aside class="sidebar">
+
+	<h3> Deaf News </h3>
+
+	<div id="body">
+	<div id="feedControl">Loading...</div>
+	</div>
+		
+</aside>
+
+<footer class="site-footer">
+ 
+<?php include('includes/footer.php');?>
+ 
+</footer>
+
+</body>
+</html>
