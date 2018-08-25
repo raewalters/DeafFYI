9b1e738134f93577268e98e1838f810680815828
raewalters <rachel.marie1184@gmail.com>
2018-08-25 18:03:28 -0400

    Create people.php

---
+++ people.php
@@ -0,0 +1,172 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <title>Deaf People | DeafFYI</title>
+  <meta name="description" content="Learn more about what it's like to be Deaf and have common questions answered.">
+  <link rel="stylesheet" href="css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+		  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+		  <script src="http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.js"
+		  type="text/javascript"></script>
+
+		  <style type="text/css">
+		  @import url("http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.css");
+
+		  #feedControl {
+		  margin-top : 5px;
+		  margin-left: 0px;
+		  margin-right: 0px;
+		  width: 300px;
+		  font-size: 10px;
+		  color: #000000;
+		  }
+		  </style>
+		  <script type="text/javascript">  
+		  function load() {
+		  var feed ="http://news.google.com/news?cf=all&hl=en&pz=1&ned=us&q=Deaf&output=rss";
+		  new GFdynamicFeedControl(feed, "feedControl");
+
+		  } 
+		  google.load("feeds", "1");
+		  google.setOnLoadCallback(load);
+		  </script>
+
+  
+</head>
+
+<body>
+
+<header class="banner">
+
+	<div class="wrapper">
+
+	<div id="banner">
+		<div id="wrapper">
+			<div id="container">
+					<img class="banner-img" src="images/header.png" alt="Deaf FYI Logo"/>
+			</div>
+		</div>
+	</div>
+	</div>
+	
+<nav class="primary-menu">
+   <ul>
+<li><a href="index.php">Home</a></li>
+      <li><a href="people.php">Deaf People</a></li>
+      <li><a href="signlanguage.php">Sign Language</a></li>
+	  <li><a href="etiquette.php">Etiquette</a></li>
+	  <li><a href="resources.php">Resources</a></li>
+	  
+   </ul>
+</nav>
+
+   </header>
+
+<main class="main">
+
+<h2>Deaf People</h2>
+
+
+
+
+<ul>
+<li><a href="#13questions">YouTube Video - 13 Questions About Being Deaf</a></li>
+<li><a href="#community">What is the Deaf Community?</a></li>
+<li><a href="#culture">What is Deaf Culture?</a></li>
+<li><a href="#bigdeaf">What is the difference between deaf and Deaf?</a></li>
+<li><a href="#readlips">Can all Deaf people read lips?</a></li>
+<li><a href="#talk">Can all Deaf people talk?</a></li>
+<li><a href="#aids">Why don’t all Deaf people wear hearing aids?</a></li>
+<li><a href="#drive">Can Deaf people drive?</a></li>
+<li><a href="#braille">Can Deaf people read braille?</a></li>
+<li><a href="#door">How do Deaf people know when someone is at the door?</a></li>
+<li><a href="#phone">How do Deaf people talk on the phone?</a></li>
+</ul>
+
+
+<h3 id="13questions">13 Questions About Being Deaf</h3>
+<div class="video-container"><iframe width="560" height="315" src="https://www.youtube.com/embed/dUzKxEzZLjQ" allowfullscreen></iframe></div>
+<p></p>
+
+<h3 id="community">What is the Deaf Community?</h3>
+<p>Each Deaf community is a cultural group which shares a sign language and a common heritage. Members of Deaf 
+communities all around the world therefore identify themselves as members of a cultural and linguistic group. 
+Identification with the Deaf community is a personal choice and is usually made independent of the individual’s 
+hearing status, and the community is not automatically composed of all people who are Deaf or hard of hearing.  
+The Deaf community may also include family members of Deaf people, sign language interpreters and people who work or 
+socialize with Deaf people who identify with Deaf culture. A person is a member of the Deaf community if he or she 
+self-identifies as a member of the Deaf community, and if other members accept that person as a member. Very often 
+this acceptance is strongly linked to competence in a signed language.</p>
+
+<h3 id="culture">What is Deaf Culture?</h3>
+<p> Deaf people as a linguistic minority have a common experience of life, and this manifests itself in Deaf culture. 
+This includes beliefs, attitudes, history, norms, values, literary traditions, and art shared by Deaf people.
+When someone is culturally Deaf if means that they use American Sign Lamnguage as their primary language and consider 
+themselves a part of the Deaf Community.</p>
+
+<h3 id="bigdeaf">What is the difference between deaf and Deaf?</h3>
+<p>Lowercase deaf is used when referring to the audiological condition of not hearing, and the uppercase Deaf when 
+referring to a particular group of deaf people who share a language – American Sign Language (ASL) – and a culture.  
+The members of this group have inherited their sign language, use it as a primary means of communication among themselves, 
+and hold a set of beliefs about themselves and their connection to the larger society.  We distinguish them from, 
+for example, those who find themselves losing their hearing because of illness, trauma or age; although these people 
+share the condition of not hearing, they do not have access to the knowledge, beliefs, and practices that make up the c
+ulture of Deaf people.</p>
+
+<h3 id="readlips">Can all Deaf people read lips?</h3>
+<p>No, not all hearing people can either! Reading lips is not something specifically that Deaf people can do, it is 
+something that Deaf people HAVE done and sometimes need to do in order to exist and participate in a conversation where 
+signing is not an option. Most people can not read lips, it is also virtually impossible to read lips accurately 100%.</p>
+
+<h3 id="talk">Can all Deaf people talk?</h3>
+<p>No, it is a personal preference whether the Deaf person prefers to use their voice or not. There is much controversy 
+surrounding this and history books to explain why. The short answer is that not all Deaf people took speech class, want 
+to speak, feel comfortable speaking or have ever used their voice. It is afterall, their choice.</p>
+
+<h3 id="aids">Why don’t all Deaf people wear hearing aids?</h3>
+<p>Many Deaf persons use hearing aids and they work great for them, for others sound amplification does not work. It 
+depends on where your hearing is in decibels. It is the individual’s choice, based on their body and preferences.</p> 
+
+<h3 id="drive">Can Deaf people drive?</h3>
+<p>Of course.</p>
+
+<h3 id="braille">Can Deaf people read braille?</h3>
+<p>Sure, if they’ve learned how to, HOWEVER, they are Deaf, not blind.</p>
+
+<h3 id="door">How do Deaf people know when someone is at the door?</h3>
+<p>Glass doors help, but in general we have doorbells that are attached to the lights, every time sometime rings the 
+doorbell the lights flash.</p>
+
+<h3 id="phone">How do Deaf people talk on the phone?</h3>
+<p>TTYs, videophones, Facetime, video texting, text messaging, Skype, etc.</p>
+
+
+<strong> Resources </strong>
+<p><a href="http://theaslapp.com/faq" target="_blank">The ASL App - "Deaf Culture FAQ"</a></p>
+<p><a href="https://nad.org/issues/american-sign-language/community-and-culture-faq" target="_blank">The National Associatation of the Deaf - "Community and Culture - Frequently Asked Questions"</a></p>
+
+
+</main>
+
+
+<aside class="sidebar">
+
+	<h3> Deaf News </h3>
+
+	<div id="body">
+	<div id="feedControl">Loading...</div>
+	</div>
+		
+</aside>
+
+<footer class="site-footer">
+ 
+<?php include('includes/footer.php');?>
+ 
+</footer>
+
+</body>
+</html>
