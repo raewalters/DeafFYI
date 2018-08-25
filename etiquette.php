bc99067002e7aa604b43a1cde4b117f2e773936d
raewalters <rachel.marie1184@gmail.com>
2018-08-25 18:02:52 -0400

    Create etiquette.php

---
+++ etiquette.php
@@ -0,0 +1,189 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <title>Etiquette | DeafFYI</title>
+  <meta name="description" content="Learn the etiquette and the do's and dont's of interacting with Deaf people.">
+  <link rel="stylesheet" href="css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+		  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+		  <script src="http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.js"
+		  type="text/javascript"></script>
+
+		  <style type="text/css">
+		  @import url("http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.css");
+
+		  #feedControl {
+		  margin-top : 5px;
+		  margin-left: 0px;
+		  margin-right: 0px;
+		  width: 300px;
+		  font-size: 10px;
+		  color: #000000;
+		  }
+		  </style>
+		  <script type="text/javascript">  
+		  function load() {
+		  var feed ="http://news.google.com/news?cf=all&hl=en&pz=1&ned=us&q=Deaf&output=rss";
+		  new GFdynamicFeedControl(feed, "feedControl");
+
+		  } 
+		  google.load("feeds", "1");
+		  google.setOnLoadCallback(load);
+		  </script>
+
+  
+</head>
+
+<body>
+
+<header class="banner">
+
+	<div class="wrapper">
+
+	<div id="banner">
+		<div id="wrapper">
+			<div id="container">
+					<img class="banner-img" src="images/header.png" alt="Deaf FYI Logo"/>
+			</div>
+		</div>
+	</div>
+	</div>
+	
+<nav class="primary-menu">
+   <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="people.php">Deaf People</a></li>
+      <li><a href="signlanguage.php">Sign Language</a></li>
+	  <li><a href="etiquette.php">Etiquette</a></li>
+	  <li><a href="resources.php">Resources</a></li>
+	</ul>
+</nav>
+
+   </header>
+
+<main class="main">
+
+<h2>Etiquette</h2>
+
+<ul>
+<li><a href="#video">YouTube Video - Deaf Culture Etiquette</a></li>
+<li><a href="#interpreter">What is an interpreter for?</a></li>
+<li><a href="#talk">How do I talk to a Deaf person when there is an interpreter?</a></li>
+<li><a href="#attention">How do I get a Deaf person’s attention?</a></li>
+<li><a href="#watch">Is it rude to watch Deaf people signing?</a></li>
+<li><a href="#walk">If Deaf people are signing and I need to walk in between them, what do I do?</a></li>
+<li><a href="#communicate">How do I know how to communicate with a Deaf person?</a></li>
+<li><a href="#dos">Do's and Dont's</a></li>
+</ul>
+
+
+<h3 id="video">Deaf Culture Etiquette</h3>
+<div class="video-container"><iframe width="560" height="315" src="https://www.youtube.com/embed/l6gPpckJ-N0" allowfullscreen></iframe></div>
+
+<h3 id="interpreter">What is an interpreter for?</h3>
+<p>To facilitate communication between a Deaf and hearing person. There are also Certified Deaf Interpreters (CDI) 
+who specialize in cultural and linguistic exchanges.</p>
+
+<h3 id="talk">How do I talk to a Deaf person when there is an interpreter?</h3>
+<p>When using an interpreter, their role is to facilitate communication between the Deaf person and the non signer. 
+They will speak in first person as the Deaf person, you should look at the Deaf person and speak directly to the them 
+instead of “tell him or her”.
+
+<h3 id="attention">How do I get a Deaf person’s attention?</h3>
+<p>Waving your arm, tapping on the shoulder, using a third person to get their attention for you, flashing lights, 
+tapping the table (to create a vibration) and tapping your foot on the floor (again, for vibrations).</p>
+
+<h3 id="watch">Is it rude to watch Deaf people signing?</h3>
+<p>Yes. It is akin to eavesdropping, except you are staring directly at the people having the conversation, who are 
+most likely VERY aware that you are staring at them. It is ok to say hi, and tell them you are learning sign language.</p>
+
+<h3 id="walk">If Deaf people are signing and I need to walk in between them, what do I do?</h3> 
+<p>You don’t need to limbo in between them, simply sign “excuse me” and walk right on through.</p>
+
+<h3 id="communicate">How do I know how to communicate with a Deaf person?</h3>
+<p>The deaf person will let you know their prefered method of communication, whether it is speaking, reading lips, 
+gesturing, signing, writing, etc. No need to worry if you don't know ASL. A pen a paper (or even a cell phone to type on)
+is acceptable. Whatever you do, Do NOT shout! Shouting does not help them understand you. </p>
+
+<h3 id="dos">Do's and Dont's</h3>
+
+<table class="maintable">
+ 
+   <caption>Do's and Dont's When Interacting with Deaf People</caption>
+ 
+    <thead>
+        <tr>
+            <th>Do</th>
+            <th>Do Not</th>
+		</tr>
+    </thead>
+    <tfoot>
+        <tr>
+            <td colspan="2">Above all be respectful!</td>
+        </tr>
+    </tfoot>
+    <tbody>
+        <tr>
+            <td>Refer to Deaf people as "Deaf" or "hard-of-hearing".</td>
+			<td>Refer to Deaf people as "deaf-mute", "deaf and dumb", or "hearing impaired". (These terms have negative connotations.)</td>
+        </tr>
+        <tr>
+            <td>Speak normally and at a normal volume.</td>
+			<td>Shout, over-enunciate, speak slower, or move your mouth in exaggerated ways.</td>
+        </tr>
+		<tr>
+            <td>Speak directly to the Deaf person.</td>
+			<td>Talk to/through the interpreter or hearing person that may be with them. </td>
+        </tr>
+		<tr>
+            <td>Talk to them like you would a hearing person. They are only Deaf.</td>
+			<td>Talk to them like they’re unintelligent.</td>
+        </tr>
+		<tr>
+            <td>Once again, talk to them normally.</td>
+			<td>Cover your mouth and then ask if they can hear you or ask them to read your lips.</td>
+        </tr>
+		<tr>
+            <td>You can interrupt if you need to, but be respectful. </td>
+			<td>If you know ASL don’t just butt in on their conversation.</td>
+        </tr>
+		<tr>
+            <td>Use a pen and paper or a cell phone to communicate.</td>
+			<td>Do not shout, speak slower, or ask them if they read lips.</td>
+        </tr>
+	</tbody>
+</table>
+
+
+
+<strong> Resources </strong>
+<p><a href="http://theaslapp.com/faq" target="_blank">The ASL App - "Deaf Culture FAQ"</a></p>
+<p><a href="http://rochellebarlow.com/13-things-to-never-say-to-a-deaf-person/" target="_blank">Rochelle Barlow - "13 Things to Never Say to a Deaf Person"</a></p>
+
+
+
+</main>
+
+
+<aside class="sidebar">
+
+	<h3> Deaf News </h3>
+
+	<div id="body">
+	<div id="feedControl">Loading...</div>
+	</div>
+		
+</aside>
+
+<footer class="site-footer">
+ 
+<?php include('includes/footer.php');?>
+ 
+</footer>
+
+
+</body>
+</html>
